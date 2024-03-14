f5fcafa3e24517e56ee0805f888fcb05ad6eb995
BalajiCode <vinothkumarbalaji150@gmail.com>
2024-03-14 09:38:13 +0530

    Update script.js

--- web_site/digital_timer/script.js
+++ web_site/digital_timer/script.js
@@ -4,9 +4,9 @@ function displayTime()
 {
     // create object for date time 
     let dateTime = new Date();
-    let hr = padZero(dateTime.getHours());
-    let min = padZero(dateTime.getMinutes());
-    let sec = padZero(dateTime.getSeconds());
+    let hr = dateTime.getHours();
+    let min = dateTime.getMinutes();
+    let sec = dateTime.getSeconds();
     // date property of js 
     let dd = dateTime.getDate();
     let mm = dateTime.getMonth()+1;
@@ -17,9 +17,9 @@ function displayTime()
         hr = hr - 12
         ampm.innerHTML = 'PM'
     }
-    document.getElementById('hour').innerHTML = hr
-    document.getElementById('minute').innerHTML = min
-    document.getElementById('secound').innerHTML = sec
+    document.getElementById('hour').innerHTML =  padZero(hr)
+    document.getElementById('minute').innerHTML =  padZero(min)
+    document.getElementById('secound').innerHTML =  padZero(sec)
     document.getElementById('day').innerHTML = addZero(dd);
     document.getElementById('month').innerHTML = addZero(mm);
     document.getElementById('year').innerHTML = yyyy;
